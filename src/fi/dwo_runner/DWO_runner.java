34a12b41b8cd574873127d6d88dcf122ad74a0ff
peterb <peterb@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2010-03-01 12:03:16 +0000

    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO_runner@2965 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- src/fi/dwo_runner/DWO_runner.java
+++ src/fi/dwo_runner/DWO_runner.java
@@ -15,11 +15,10 @@ import java.lang.reflect.InvocationTargetException;
             //if (args.length < 1) {
             //    usage();
             //}
-            args = new String[4];
+            args = new String[2];
             args[0] = "http://www.fi.uu.nl/dwo/jars/dwo.jar";
-            args[1] = "20";
-            args[2] = "admin";
-            args[3] = "gvt-33";
+            args[1] = "50";
+            
             
             
             URL url = null;
