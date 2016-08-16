c04209a8bc56702bb405bde9eff60e9ecd835eb5
velth101 <velth101@15dc1663-c6ff-0310-8f09-83f172d7987a>
2016-08-16 10:47:18 +0000

    met de laatste felix
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.projects/MicroServer@796 15dc1663-c6ff-0310-8f09-83f172d7987a

--- src/main/java/fi/microserver/MicroServer.java
+++ src/main/java/fi/microserver/MicroServer.java
@@ -1,5 +1,6 @@
 package fi.microserver;
 
+import java.util.Date;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -10,9 +11,10 @@ import java.util.ServiceLoader;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
-import org.apache.felix.httplite.osgi.Activator;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
 import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
@@ -30,52 +32,81 @@ import fi.microserver.servlet.StatusServlet;
 
 public class MicroServer {
 
+	private static final String DWOJAPPLET = "file:///Users/wim/Documents/workspace-luna/DWOJApplet/target/" + "DWOJApplet-2.0-SNAPSHOT-jar-with-dependencies.jar" ;
+	private static final String CONSOLE = "file://" +"/Users/wim/Downloads/concierge-incubation-5.0.0/bundles/" + "org.eclipse.concierge.shell-5.0.0.20151029184259.jar";
 	private static Framework framework;
+	private static BundleContext context;
 	private static LogService logger;
 	private static Configurator cm;
+	private static Bundle dwo;
+	private static Bundle console;
 
 	public static void main(String[] args) throws Exception {
 
 		FrameworkFactory factory = ServiceLoader.load(FrameworkFactory.class).iterator().next();
 		Map<String, String> map = new HashMap<String,String>();
+		//map.put(Constants.FRAMEWORK_STORAGE_CLEAN,Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
+		map.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, "javax.swing,javax.swing.border,javax.swing.event,javax.swing.filechooser,javax.swing.plaf,javax.swing.plaf.basic,javax.swing.plaf.metal,javax.swing.table,javax.swing.text,javax.swing.text.html,javax.swing.tree");
+		map.put("fi.dwo.profile", "77");
 		framework = factory.newFramework(map);
-		
 		framework.init();
-						
+		context = framework.getBundleContext();
 		try {
 			framework.start();
+			installConsole();
 			installServlet();
 			installLogging();
 			installConfigurator();
-			installBrowser();
+			installDWO();
+			//installBrowser();
 			framework.waitForStop(0);
 		} catch (InterruptedException e) {
 		} finally {
 			System.exit(0);
-		}
-		
+		}	
 	}
 
+	private static void installDWO() {
+		try {
+			dwo = context.getBundle(DWOJAPPLET);
+			if(dwo == null)
+				dwo = context.installBundle(DWOJAPPLET);
+			else 
+			{
+				long modified = dwo.getLastModified();
+				System.out.println(new Date(modified));
+				if(false) dwo.update();
+			}
+			dwo.start();
+		} catch (Exception e) {
+			logger.log(LogService.LOG_ERROR, "installing DWO", e);
+		}
+	}
+	
+	private static void installConsole() throws BundleException {
+		console = context.getBundle(CONSOLE);
+		if(console == null)
+			console = context.installBundle(CONSOLE);
+		console.start();
+	}
+	
 	private static void installBrowser() {
-		Browser browser = new Browser(logger);
-		
+		Browser browser = new Browser(logger);		
 		browser.start("status");
 	}
 
 	private static void installLogging() {
 		logger = new Logger();
 		Dictionary<String, ?> dict = new Hashtable<String,String>();
-		framework.getBundleContext().registerService(LogService.class, logger, dict);
+		context.registerService(LogService.class, logger, dict);
 	}
 
 	private static void installConfigurator() {
-		cm = new Configurator(framework.getBundleContext(), logger);
+		cm = new Configurator(context, logger);
 		cm.open();
 	}
-	
-	
+		
 	private static void installServlet() {
-		final BundleContext context = framework.getBundleContext();
 
 		ServiceTracker<HttpService,HttpService> tracker;
 		ServiceTrackerCustomizer<HttpService, HttpService> customizer = new ServiceTrackerCustomizer<HttpService, HttpService>() {
@@ -86,7 +117,6 @@ public class MicroServer {
 
 			{ 
 				initparams.put("status", "from installServlet");
-	
 			}
 
 			public HttpService addingService(ServiceReference<HttpService> ref) {
