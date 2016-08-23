fc9034500a6260bab69da38fc1d9dba13b47f1d5
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2016-08-23 16:26:37 +0000

    met icoontjes
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO_runner@13798 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/mac-deploy.sh
+++ toolfiles/mac-deploy.sh
@@ -6,20 +6,22 @@ rm -rf deploy
 jh=$(/usr/libexec/java_home -v 1.8)
 export JAVA_HOME=$jh
 name=DWO-docent
+disk=DWO-setup
 build="-srcfiles output/jar/dwo_runner.jar"
 $jh/bin/javapackager -deploy \
 	-BappVersion=2.0 \
 	-Bruntime="$jh/../../" \
-	-BsystemWide=false \
+	-BsystemWide=true \
 	-BjvmOptions=-Xmx1024m \
 	-nosign \
 	-native dmg \
 	-name $name \
-	-title $name \
+	-title $name-2 \
 	-vendor Numworx \
 	-description "Start the DWO" \
 	-height 600 -width 800 \
 	-appclass fi.dwo_runner.DWO_runner \
 	$build \
 	-outdir deploy \
-	-outfile $name
+	-outfile $disk \
+	-verbose
