03b3f7055978bf214ef8365df25a569d13d7e88b
velth101 <velth101@15dc1663-c6ff-0310-8f09-83f172d7987a>
2016-08-29 20:25:20 +0000

    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.projects/MicroServer@807 15dc1663-c6ff-0310-8f09-83f172d7987a

--- src/main/java/fi/microserver/MicroServer.java
+++ src/main/java/fi/microserver/MicroServer.java
@@ -1,10 +1,13 @@
 package fi.microserver;
 
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.ServiceLoader;
+
 import javax.swing.JOptionPane;
 
 import org.osgi.framework.Bundle;
@@ -17,29 +20,32 @@ import org.osgi.framework.launch.FrameworkFactory;
 
 public class MicroServer {
 
-	private static  String CODEBASE = "file://" + "/opt/dwo/webapps/war-shared/jars/";
-	private static  String BOOT = "file://" + "/Users/wim/Documents/workspace-luna/BootLoader/target/" + "BootLoader-0.0.1-SNAPSHOT.jar";
 	private static Framework framework;
 	private static BundleContext context;
 	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
 		
-		CODEBASE = "https://app.dwo.nl/dwo/jars/";
-		
 		FrameworkFactory factory = ServiceLoader.load(FrameworkFactory.class).iterator().next();
+
 		Map<String, String> map = new HashMap<String,String>();
 	    //map.put(Constants.FRAMEWORK_STORAGE_CLEAN,Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
-		//map.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, "javax.swing,javax.swing.border,javax.swing.event,javax.swing.filechooser,javax.swing.plaf,javax.swing.plaf.basic,javax.swing.plaf.metal,javax.swing.table,javax.swing.text,javax.swing.text.html,javax.swing.tree");
 		map.put(Constants.FRAMEWORK_STORAGE, System.getProperty("user.home") + "/felix-cache");
 		map.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, "javafx.application,javafx.beans.property,javafx.beans.value,javafx.collections,javafx.concurrent,javafx.embed.swing,javafx.event,javafx.scene,javafx.scene.control,javafx.scene.web,javafx.util,javax.swing,javax.swing.border,netscape.javascript"
 				+ ",com.apple.eawt"
 				+ ",org.osgi.service.cm;version=1.5, org.osgi.service.log;version=1.3"
 
 			);
-		map.put("fi.dwo.profile", "1");
-		map.put("fi.dwo.language", "nl");
-		map.put("fi.dwo.codebase", CODEBASE);
-
+		Properties props = new Properties();
+		InputStream in = MicroServer.class.getResourceAsStream("resources/DWO.properties");
+		props.load(in);
+		in.close();
+		map.putAll((Map)props);
+		if("true".equals(props.getProperty("fi.dwo.properties")))
+			map.put("fi.dwo.documentbase", MicroServer.class.getResource("resources/").toExternalForm());
+		else 
+			map.remove("fi.dwo.properties");
+		
 		framework = factory.newFramework(map);
 		framework.init();
 		context = framework.getBundleContext();
@@ -62,20 +68,15 @@ public class MicroServer {
 	}
 
 	private static void installBoot() throws BundleException {
+		String BOOT = context.getProperty("fi.dwo.boot");
 		Bundle bootloader = context.installBundle(BOOT);
 		bootloader.start();
 	}
 
-	private static void installWrap() {
+	private static void installWrap() throws Exception {
 		BundleActivator activator;
-		
-		try {
-			activator = new org.ops4j.pax.url.wrap.internal.Activator();
-			activator.start(context);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		activator = new org.ops4j.pax.url.wrap.internal.Activator();
+		activator.start(context);
 	}
 	
 }
