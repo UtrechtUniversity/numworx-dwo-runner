dabda224dda340107210c0214a3dac130979a982
velth101 <velth101@15dc1663-c6ff-0310-8f09-83f172d7987a>
2016-10-20 16:52:30 +0000

    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.projects/MicroServer@852 15dc1663-c6ff-0310-8f09-83f172d7987a

--- win-deploy.bat
+++ win-deploy.bat
@@ -3,4 +3,4 @@ set JRE_HOME="%JAVA_HOME%\jre"
 set JRE_HOME=C:\Program Files\Java\jre1.8.0_101
 path %JAVA_HOME%\bin;C:\Program Files (x86)\Inno Setup 5;%PATH%
 
-javapackager -deploy -BappVersion=0.0.1 -Bruntime="%JRE_HOME%" -BjvmOptions=-Xmx1024m -native exe -name MicroServer -title MicroServer -vendor Numworx -description "Start the DME" -height 600 -width 800 -appclass fi.microserver.MicroServer -srcdir target -outdir deploy -outfile MicroServer
+javapackager -deploy -BappVersion=0.0.1 -Bruntime="%JRE_HOME%" -BjvmOptions=-Xmx1024m -native exe -name DWO-docent -title DWO-docent -vendor Numworx -description "Start the DME" -height 600 -width 800 -appclass fi.microserver.MicroServer -srcdir target -outdir deploy -outfile DWO-docent-install
