85524eb659787dce10490dba1ba6f87e02f4c77a
velth101 <velth101@15dc1663-c6ff-0310-8f09-83f172d7987a>
2017-12-21 13:37:23 +0000

    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.projects/MicroServer@948 15dc1663-c6ff-0310-8f09-83f172d7987a

--- linux-deploy.sh
+++ linux-deploy.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 rm -rf deploy
 #jh=$(/usr/libexec/java_home -v 1.8)
-jh=~/java
+jh=/usr/lib/jvm/oracle_jdk8
 export JAVA_HOME=$jh
 build=$(for i in target/*.jar; do /bin/echo -n " -srcfiles " $i; done)
 $jh/bin/javapackager -deploy \
@@ -17,7 +17,7 @@ $jh/bin/javapackager -deploy \
 	-vendor Numworx \
 	-description "Start the DWO" \
 	-height 600 -width 800 \
-	-appclass fi.microserver.MicroServer \
+	-appclass fi.microserver.DWO \
 	$build \
 	-outdir deploy \
 	-outfile DWO-docent
