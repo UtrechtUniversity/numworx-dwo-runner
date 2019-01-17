b5a2f1dc7566c9e52a9c62440d147f7640d91eda
velth101 <velth101@15dc1663-c6ff-0310-8f09-83f172d7987a>
2019-01-17 09:21:52 +0000

    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.projects/MicroServer@1042 15dc1663-c6ff-0310-8f09-83f172d7987a

---
+++ runtime11.sh
@@ -0,0 +1,7 @@
+set -x
+cd target
+x=$(jdeps --print-module-deps MicroServer-0.0.4.jar dependency/*.jar)
+echo $x
+x='java.base,java.desktop,java.naming,java.prefs,java.scripting,java.sql,java.xml,jdk.jsobject'
+rm -rf java-runtime
+jlink -v --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $x --output java-runtime
