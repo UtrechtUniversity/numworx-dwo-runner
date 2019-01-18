c11343dc06daead07287fc8af9dd2949a917c78d
velth101 <velth101@15dc1663-c6ff-0310-8f09-83f172d7987a>
2019-01-18 14:08:47 +0000

    javafx mogelijk optioneel
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.projects/MicroServer@1044 15dc1663-c6ff-0310-8f09-83f172d7987a

--- src/main/java/fi/microserver/MicroServer.java
+++ src/main/java/fi/microserver/MicroServer.java
@@ -85,8 +85,9 @@ public class MicroServer {
 	    else if(isMac) dir += File.separator + "Library" + File.separator + "Application Support";
 
 		map.put(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, 
-		  "javafx.application,javafx.beans.property,javafx.beans.value,javafx.collections,javafx.concurrent,javafx.embed.swing,javafx.event,javafx.scene,javafx.scene.control,javafx.scene.web,javafx.util,javax.swing,javax.swing.border,netscape.javascript"
-				+ ",com.apple.eawt"
+// Java 8,9 en 10, 11 alleen met een eigen java
+		  "javafx.application,javafx.beans.property,javafx.beans.value,javafx.collections,javafx.concurrent,javafx.embed.swing,javafx.event,javafx.scene,javafx.scene.control,javafx.scene.web,javafx.util,javax.swing,javax.swing.border,netscape.javascript," + 
+				"com.apple.eawt"
 				+ ",org.osgi.service.cm;version=1.5"
 				+ ",org.osgi.service.log;version=1.3"
 				+ ",org.osgi.service.event;version=1.3.1"
