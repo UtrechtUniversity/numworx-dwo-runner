36b4dd44be826259cebd5add73ef8ff765b83632
velth101 <velth101@58dd2db4-0f11-0410-9df4-da15f00f50bd>
2019-12-03 15:48:13 +0000

    executable is zulu-8 met javaFX en losse JRE (met javaFX)
    git-svn-id: https://svn.science.uu.nl/repos/project.fisme.java/DWO_runner@24066 58dd2db4-0f11-0410-9df4-da15f00f50bd

--- toolfiles/mac-deploy.sh
+++ toolfiles/mac-deploy.sh
@@ -3,23 +3,24 @@ cd ..
 set -e
 set -x
 rm -rf deploy
-jh=$(/usr/libexec/java_home -v 1.8.0_201)
+jh=/Library/Java/JavaVirtualMachines/zulu-8.jdk/Contents/Home
+rt=~/zulu-8.jre/
 export JAVA_HOME=$jh
-name=NumworxAuthor-TEST
-disk=Author-TEST-setup
-build=$(for i in DWO.properties target/*.jar target/dependency/*.jar; do /bin/echo -n " -srcfiles " $i; done)
+name=NumworxAuthor
+disk=NumworxAuthor-setup
+build=$(for i in target/*.jar target/dependency/*.jar; do /bin/echo -n " -srcfiles " $i; done)
 
 $jh/bin/javapackager -deploy \
-	-BappVersion=2.0 \
-	-Bruntime="$jh/../../" \
+	-BappVersion=2.4 \
+	-Bruntime="$rt" \
 	-BsystemWide=true \
 	-BjvmOptions=-Xmx1024m \
 	-nosign \
 	-native dmg \
 	-name $name \
-	-title $name-2 \
+	-title $name \
 	-vendor Numworx \
-	-description "Start the DWO" \
+	-description "Start Numworx Author" \
 	-height 600 -width 800 \
 	-appclass fi.dwo_runner.DWO_runner \
 	$build \
