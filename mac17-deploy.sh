9a6c444236d59d67e2a56e13b8e05bbcaa5e41bd
velth101 <velth101@15dc1663-c6ff-0310-8f09-83f172d7987a>
2021-09-27 08:18:25 +0000

    java 17 support voor applicaties, mac
    optional jxbrowser profile
    
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.projects/MicroServer@1264 15dc1663-c6ff-0310-8f09-83f172d7987a

---
+++ mac17-deploy.sh
@@ -0,0 +1,11 @@
+jh=$(/usr/libexec/java_home -v 17)
+JAVA_HOME=$jh $jh/bin/jpackage --help 
+
+
+
+cp target/MicroServer*.jar target/dependency
+rm -rf target/deploy/*
+JAVA_HOME=$jh $jh/bin/jpackage  -t app-image -n NumworxAuthor -d target/deploy -i target/dependency \
+  --main-jar MicroServer-0.0.7-SNAPSHOT.jar --app-version 1.0.7 \
+  --java-options --add-exports=java.desktop/sun.awt.www.content.image=ALL-UNNAMED \
+  --verbose --vendor Numworx 
