0a909a01548d680bc4d28dc170ab9b59af562aeb
velth101 <velth101@32ccb885-960d-4b53-a61f-10870b223014>
2022-02-03 10:30:45 +0000

    nieuw jaar 2022
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.numworx/sessionupdate/trunk@1039 32ccb885-960d-4b53-a61f-10870b223014

--- src/main/java/nl/numworx/elk/sessionupdate/Main.java
+++ src/main/java/nl/numworx/elk/sessionupdate/Main.java
@@ -74,8 +74,8 @@ for(int m = 8; m < 12; m++ ) {
 		SearchRequest searchRequest = new SearchRequest("logstash-*"); 
 		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder(); 
 		QueryBuilder query = new ExistsQueryBuilder("user_id");
-		Date fromdate = new Date(2021-1900,m  ,1,0,0,0);
-		Date todate   = new Date(2021-1900,m+1,1,0,0,0);
+		Date fromdate = new Date(2022-1900,m  ,1,0,0,0);
+		Date todate   = new Date(2022-1900,m+1,1,0,0,0);
 		QueryBuilder start = new RangeQueryBuilder("@timestamp").from(fromdate, true).to(todate, false);
 		searchSourceBuilder.query(new BoolQueryBuilder().must(query)
 				.must(start)
