1fd1bed9d935dd10c01bd5acb0b0a093cc8a8bb0
velth101 <velth101@15dc1663-c6ff-0310-8f09-83f172d7987a>
2022-12-08 06:14:03 +0000

    fix imports
    manifest obsolete, geen jxbrowser
    
    git-svn-id: https://svn.science.uu.nl/repos/sci.velth101.projects/MicroServer@1297 15dc1663-c6ff-0310-8f09-83f172d7987a

--- src/main/java/fi/microserver/MicroServer.java
+++ src/main/java/fi/microserver/MicroServer.java
@@ -1,9 +1,7 @@
 package fi.microserver;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.reflect.InvocationTargetException;
@@ -23,7 +21,6 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
-import org.apache.felix.framework.Logger;
 import org.apache.felix.framework.util.FelixConstants;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
