138bd97540be34076d0df8c87710625235b4995d
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2023-06-23 08:54:31 +0200

    aws naar azure kopieer routines

---
+++ AWSscripts/aws3appnaarazure.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export AWS_ACCESS_KEY_ID=key
+export AWS_SECRET_ACCESS_KEY=secret
+export KEY=azure
+. $HOME/aws.env
+EXP=$(date -v+1d +%Y-%m-%d)
+echo $EXP
+SAS=$(az storage container generate-sas --account-name numworxprod --name cds  --auth-mode key  --permissions dlrw --expiry $EXP --account-key $KEY)
+SAS=$(echo $SAS|tr -d '"')
+SRC=https://s3.eu-west-1.amazonaws.com/cds.dwo.nl/
+azcopy copy $SRC https://numworxprod.blob.core.windows.net/cds/?"$SAS" --recursive=true
+
