b52cf7f531c149a11e3893a74175614c38bfa0e7
Velthoven, W.P.G. van (Wim) <w.p.g.vanvelthoven@uu.nl>
2023-06-27 14:19:13 +0200

    DWOOffline met Numworx

---
+++ DWOOffline/src/main/java/nl/numworx/dwooffline/RestOffline.java
@@ -0,0 +1,20 @@
+package nl.numworx.dwooffline;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RestOffline extends HttpServlet {
+	private static final String ERROR = "{\"msg\":\"offline\",\"Dwo2ExceptionCode\":\"Rest_CanNotReachServer\"}";
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendError(400);
+		resp.getWriter().write(ERROR);
+	}
+	
+	
+}
--- DWOOffline/src/main/webapp/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-<h2>DWO onderhoud</h2>
-<p>Op dit moment wordt er onderhoud gedaan. Alle services zijn offline</p>
-<h2>DME maintenance</h2>
-<p>At this moment the DME is in maintenance. All services are offline
-</body>
-</html>
