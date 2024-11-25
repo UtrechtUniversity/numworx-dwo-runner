14dcbbc42d7bfdfbffb701a1daabb4da9beef974
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2024-11-25 23:33:27 +0100

    betere versie nl Bundle-Version

--- osgi/DWOJApplet-Starter/src/main/java/fi/dwo/dwojapplet/boot/EAWTTracker.java
+++ osgi/DWOJApplet-Starter/src/main/java/fi/dwo/dwojapplet/boot/EAWTTracker.java
@@ -7,6 +7,7 @@ import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JDialog;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
@@ -18,14 +19,22 @@ import fi.dwo.eawt.EAWT;
 
 public class EAWTTracker extends ServiceTracker<EAWT, EAWT> implements Supplier<JDialog> {
 	
+	private static final String SYMB = "fi.dwo.dwojapplet.domain.DWO";
+	
 	final String version;
 	final String title;
 	final MainFrame frame;
 	final BooleanSupplier quit;
+	String DWOversion = version();;
 
 	public EAWTTracker(BundleContext context, MainFrame mainFrame, BooleanSupplier quit) {
 		super(context, "fi.dwo.eawt.EAWT", null);
 		version = context.getProperty("fi.microserver.version");
+		Bundle[] bs = context.getBundles();
+		for(Bundle i : bs) {
+			if (SYMB .equals(i.getSymbolicName())) DWOversion = i.getVersion().toString();
+		}
+		
 		title = "Numworx Author";
 		frame = mainFrame;
 		this.quit = quit;
@@ -45,7 +54,8 @@ public class EAWTTracker extends ServiceTracker<EAWT, EAWT> implements Supplier<
 		JLabel title = new JLabel(this.title);
 		title.setFont(title.getFont().deriveFont(18.0f));
 		JLabel runner = new JLabel("Runner Version: " + version);
-		JLabel dwo    = new JLabel("Author Version: " + BUILD.version);
+		JLabel dwo    = new JLabel("Author Version: " + DWOversion);
+		JLabel java   = new JLabel("Java Version: " + Runtime.version());
 		Box box = Box.createVerticalBox();
 		box.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 		
@@ -54,11 +64,20 @@ public class EAWTTracker extends ServiceTracker<EAWT, EAWT> implements Supplier<
 		box.add(Box.createVerticalStrut(10));
 		box.add(runner);
 		box.add(dwo);
+		box.add(java);
 		dialog.setModal(true);
 		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		dialog.pack();
 		dialog.setLocationRelativeTo(frame);
 		return dialog;
+	}
+
+	protected String version() {
+		try {
+			return BUILD.version; // mogelijk no such class exception
+		} catch (Throwable e) {
+			return e.toString();
+		}
 	}
 
 }
