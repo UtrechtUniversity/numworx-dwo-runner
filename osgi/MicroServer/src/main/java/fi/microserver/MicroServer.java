b33bc1cf52c4966f64768b72c3e283f1b974341a
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-05-21 16:15:06 +0200

    geen idee

--- osgi/MicroServer/src/main/java/fi/microserver/MicroServer.java
+++ osgi/MicroServer/src/main/java/fi/microserver/MicroServer.java
@@ -47,7 +47,6 @@ public class MicroServer {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void main(String[] args) throws Exception {
 	    System.setProperty("apple.eawt.quitStrategy", "CLOSE_ALL_WINDOWS");
-	    System.setProperty("java.security.policy","all.policy");
 	    
 		Preferences pref = Preferences.userRoot().node("fi/microserver");
 		String uuid = pref.get("uuid", null);
