7cd1700c2ec7aef57b575420d5eb6d31ebbaa0b9
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-05-28 11:50:27 +0200

    Met bundles

--- AWSscripts/aws3appnaarbackup.sh
+++ AWSscripts/aws3appnaarbackup.sh
@@ -2,3 +2,4 @@ DRY=--dryrun
 if [ "$1" = "--go" ]; then DRY=; fi
 aws --profile prod s3 sync s3://cds.dwo.nl/apps/ s3://cds-s3-backup/apps/ $DRY --delete
 aws --profile prod s3 sync s3://cds.dwo.nl/jars/ s3://cds-s3-backup/jars/ $DRY --delete
+aws --profile prod s3 sync s3://cds.dwo.nl/bundles/ s3://cds-s3-backup/bundles/ $DRY --delete
