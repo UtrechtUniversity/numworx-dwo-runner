f8a85a30eb098e4a33c33ceeb0ff1dd2787c63dc
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-12-08 15:39:17 +0100

    with - instead of .

--- go.sh
+++ go.sh
@@ -1,16 +1,16 @@
 cd target
 ../bnd ../swing.bnd
-../inst com.teamdev.jxbrowser.swing
+../inst com.teamdev.jxbrowser-swing
 ../bnd ../core.bnd
 ../inst com.teamdev.jxbrowser
 ../bnd ../mac.bnd
-../inst com.teamdev.jxbrowser.mac
+../inst com.teamdev.jxbrowser-mac
 ../bnd ../mac-arm.bnd
-../inst com.teamdev.jxbrowser.mac-arm
+../inst com.teamdev.jxbrowser-mac-arm
 ../bnd ../linux.bnd
-../inst com.teamdev.jxbrowser.linux64
+../inst com.teamdev.jxbrowser-linux64
 ../bnd ../windows.bnd
-../inst com.teamdev.jxbrowser.win64
+../inst com.teamdev.jxbrowser-win64
 ../bnd ../linux-arm.bnd
-../inst com.teamdev.jxbrowser.linux64-arm
+../inst com.teamdev.jxbrowser-linux64-arm
 
