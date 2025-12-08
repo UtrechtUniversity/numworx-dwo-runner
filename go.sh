dc9b38ceecf1be74c508d014ff71a760bf255b8d
Wim van Velthoven <w.p.g.vanvelthoven@uu.nl>
2025-12-08 15:28:02 +0100

    build and install into local repository

--- go.sh
+++ go.sh
@@ -1,4 +1,16 @@
-./bnd swing.bnd
-./bnd core.bnd
-./bnd mac.bnd
-./bnd mac-arm.bnd
+cd target
+../bnd ../swing.bnd
+../inst com.teamdev.jxbrowser.swing
+../bnd ../core.bnd
+../inst com.teamdev.jxbrowser
+../bnd ../mac.bnd
+../inst com.teamdev.jxbrowser.mac
+../bnd ../mac-arm.bnd
+../inst com.teamdev.jxbrowser.mac-arm
+../bnd ../linux.bnd
+../inst com.teamdev.jxbrowser.linux64
+../bnd ../windows.bnd
+../inst com.teamdev.jxbrowser.win64
+../bnd ../linux-arm.bnd
+../inst com.teamdev.jxbrowser.linux64-arm
+
